b321a1154b7d0914f4b9b90c220b84165fac6d36
logiczny <33063528+logiczny@users.noreply.github.com>
2022-12-07 18:36:42 +0100

    wycinka2

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"2Ap2EB7T%P+(pb^I~p-`,Xr|+`|VS1v.T.p8+ZnwVq.,YJ#[7`*kZ5qv]&!E3e!<";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3881;}
