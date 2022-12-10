31c1a7055b7cc5b883e6a8ba9963a20cec54ad71
logiczny <33063528+logiczny@users.noreply.github.com>
2022-12-10 14:38:03 +0100

    var names corrected to english

--- otherStuff/skrypt.sh
+++ otherStuff/skrypt.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 # test api token from wpscan, you can put more here like so: ("1" "2" "3")
 apiTokensList="otherStuff/apiKeys.list"
-apiToken=$(shuf -n 1 "$apiTokensList")
+apiTokenChoosed=$(shuf -n 1 "$apiTokensList")
 
-skanowanie()
+f_scan()
 (
 	if [ "$5" = "-A" ]; then
-	wytnijSpam1="Service detection performed. Please report any incorrect results at https://nmap.org/submit/"
-	wytnijSpam2="Nmap done: 1 IP address"
-	wytnijSpam3="Starting Nmap 7.70 ( https://nmap.org )"
-	nmap "$1" -p"$2"-"$3" "$5" --script=default,vuln | grep -v "$wytnijSpam1" | grep -v "$wytnijSpam2" | grep -v "$wytnijSpam3" 2>&1
+	cutSpam1="Service detection performed. Please report any incorrect results at https://nmap.org/submit/"
+	cutSpam2="Nmap done: 1 IP address"
+	cutSpam3="Starting Nmap 7.70 ( https://nmap.org )"
+	nmap "$1" -p"$2"-"$3" "$5" --script=default,vuln | grep -v "$cutSpam1" | grep -v "$cutSpam2" | grep -v "$cutSpam3" 2>&1
 	fi
 	### remember to extend php cli & fpm timeouts, also nginx
 	### https://easyengine.io/tutorials/php/increase-script-execution-time/
 
 	IFS=$'\n'; nmapOutput=( $(nmap "$1" -p"$2"-"$3" | grep open) )
-	declare -a otwartePorty;
+	declare -a openPorts;
+	if [ -n "$nmapOutput" ]; then
+		echo "Open ports found:"
+	fi
 
 	for i in "${!nmapOutput[@]}"; do
 		shortening=$(echo "${nmapOutput[i]}" | sed 's#/.*##g')
-		otwartePorty[i]="$shortening";
-		echo "${otwartePorty[i]}";
+		openPorts[i]="$shortening";
+		echo "${openPorts[i]}";
 	done
 
-	if [ -n "$otwartePorty" ]; then
-		for i in "${otwartePorty[@]}"; do
+	if [ -n "$openPorts" ]; then
+		for i in "${openPorts[@]}"; do
 			echo "WPscan per port starts now";
-			wpscan --url http://${1}:${i} -f cli-no-color --api-token ${apiToken} --no-banner --random-user-agent "$4";
-			wpscan --url https://${1}:${i} -f cli-no-color --api-token ${apiToken} --no-banner --random-user-agent "$4";
+			wpscan --url http://${1}:${i} -f cli-no-color --api-token ${apiTokenChoosed} --no-banner --random-user-agent "$4";
+			wpscan --url https://${1}:${i} -f cli-no-color --api-token ${apiTokenChoosed} --no-banner --random-user-agent "$4";
 		done
 	else
 		echo "Nothing found, try another ports?"
 	fi
 )
-echo "Start skanowania"
-#echo $5
+echo "Scan started for $1"
 if [ $# -eq 5 ]; then
-    skanowanie $1 $2 $3 "$4" "$5"
+    f_scan $1 $2 $3 "$4" "$5"
 fi
 
