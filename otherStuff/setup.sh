c8927a6f0f0a80a11d4992e480b7fe2176398447
logiczny <33063528+logiczny@users.noreply.github.com>
2022-12-10 12:03:17 +0100

    dependencies

--- otherStuff/setup.sh
+++ otherStuff/setup.sh
@@ -1,5 +1,7 @@
- #!/bin/bash
+#!/bin/bash
 sudo apt install ruby 
-sudo apt install build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev  libgmp-dev zlib1g-dev git
-sudo apt install nginx 
+sudo apt install build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev libgmp-dev zlib1g-dev
+sudo apt install nginx php-fpm php-mysql mariadb-server nmap
 sudo gem install wpscan
+
+
